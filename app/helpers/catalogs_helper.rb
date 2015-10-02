4d9de425e3471cc813b5a7116bacfc2aee969790
ynzkai <ynzkai@gmail.com>
2015-10-02 23:23:11 +0800

    change catalog helper

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,6 +1,6 @@
 module CatalogsHelper
 
-  # 枚举某一节点下的所有分类
+  # 枚举某一节点下的所有分类 
   # 示例: <%= get_catalogs_html('分类') %>
 
   def get_catalogs_html(name)
@@ -8,18 +8,9 @@ module CatalogsHelper
     return '' if cata.nil?
 
     if cata.is_parent?
-      catalogs = Catalog.where(parent_id: cata.id)
       html = '<ul><li>' + name + '</li>'
-
-      if catalogs.count > 0
-      #Catalog.where(parent_id: cata.id).each do |c|
-        html += '<ul>'
-        catalogs.each do |c|
-          #html += get_catalogs_html(c.name)
+      Catalog.where(parent_id: cata.id).each do |c|
           html += get_catalogs_html(c.name)
-        end
-        html += '</ul>'
-      #end
       end
       html += '</ul>'
     else
