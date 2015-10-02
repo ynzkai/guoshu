3c00818f571b3a284103c1f6c56aa095705f73cf
ynzkai <ynzkai@gmail.com>
2015-10-02 03:38:58 -0700

    design home views

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,16 +1,25 @@
 module CatalogsHelper
 
-  # 枚举某一节点下的所有分类,需将其放在<ul>标签中
-  # 示例: <ul><%= get_catalogs_html('分类') %></ul>
+  # 枚举某一节点下的所有分类
+  # 示例: <%= get_catalogs_html('分类') %>
 
   def get_catalogs_html(name)
     cata = Catalog.find_by name: name
     return '' if cata.nil?
 
     if cata.is_parent?
-      html = '<li>' + name + '</li><ul>'
-      Catalog.where(parent_id: cata.id).each do |c|
-        html += get_catalogs_html(c.name)
+      catalogs = Catalog.where(parent_id: cata.id)
+      html = '<ul><li>' + name + '</li>'
+
+      if catalogs.count > 0
+      #Catalog.where(parent_id: cata.id).each do |c|
+        html += '<ul>'
+        catalogs.each do |c|
+          #html += get_catalogs_html(c.name)
+          html += get_catalogs_html(c.name)
+        end
+        html += '</ul>'
+      #end
       end
       html += '</ul>'
     else
