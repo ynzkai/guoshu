b9589e5527427951bcc2e51cab0f97d3b09a2aad
ynzkai <ynzkai@gmail.com>
2015-10-20 22:51:42 +0800

    change home page css & add foot

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,10 +1,22 @@
 $(function () {
 	$(".addtocart-button").click(function () {
-		$product = $(this).parent(".buy").children("#product_id") ||
-				   $(this).parent().parent(".buy").children("#product_id");
 		$.post('/line_items.json', {
-				buycount: ($("#buycount").val() || 1),
-				product_id: $product.val()
+				buycount: $("#buycount").val(),
+				product_id: $("#product_id").val()
+			},
+			function(result, status) {
+				$("#cart_item_count").html(result.count);
+				$("#cart").addClass('animated shake');
+				alert("已将该商品加入购物车");
+		});
+		$("#cart").removeClass('animated shake');
+		return false;
+	});
+
+	$(".addtocart-button-sm").click(function () {
+		$.post('/line_items.json', {
+				buycount: 1,
+				product_id: $(this).parent(".buy").children("#product_id").val()
 			},
 			function(result, status) {
 				$("#cart_item_count").html(result.count);
