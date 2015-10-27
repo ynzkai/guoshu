2ac6850d7222ffd67efdf101bc416e72db8f164d
ynzkai <ynzkai@gmail.com>
2015-10-27 00:44:35 -0700

    add user page css

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_one :cart, dependent: :destroy
   has_one :reciever, dependent: :destroy
   has_many :orders, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
 
 
